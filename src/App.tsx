b5aadec9dc9f3205a0b2d6329d5e5b4e619fc2a7
dmesa <dm31284@gmail.com>
2024-04-14 18:15:50 -0500

    put app route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './configs';
+import { Router } from './router';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Router />
     </ThemeProvider>
   );
 };
