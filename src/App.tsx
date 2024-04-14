31a78d0472fb5b59262d91d732703202c2125e49
IsabellaSolarte <isabsolarte@unicauca.edu.co>
2024-04-13 22:43:03 -0500

    Se finalizó el input

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './configs';
 import { Router } from './router';
-import CustomButton from './components/Atoms/Button/Button';
-import AddIcon from '@mui/icons-material/Add';
+import CustomInput from './components/Atoms/Inputs/Input';
+
 
 const App = () => {
   return (
