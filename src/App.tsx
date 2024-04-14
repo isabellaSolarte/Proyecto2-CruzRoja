fd79636e8677cd8c87f3a29681726e8bc5b63e26
IsabellaSolarte <isabsolarte@unicauca.edu.co>
2024-04-14 17:48:33 -0500

    Prueba

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './configs';
 import { TabsAtomComponent } from './components/Atoms/Tabs';
+import { CustomButton } from './components';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <TabsAtomComponent tabContentItem={['s', 's']} />
+      <TabsAtomComponent tabContentItem={['siu', 'no']} />
+      <CustomButton content="Ejemplo" variant="contained" color="info" sx={{ fontSize: '16px', padding: '5px'}}/>
     </ThemeProvider>
   );
 };
