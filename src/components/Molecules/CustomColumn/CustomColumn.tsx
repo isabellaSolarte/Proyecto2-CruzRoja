593a9da0c8d4694eef23770e4e2de02fcfd84f00
Mditran <micheldiaz@unicauca.edu.co>
2024-04-14 18:36:28 -0500

    Se modifico el customColumn

--- src/components/Molecules/CustomColumn/CustomColumn.tsx
+++ src/components/Molecules/CustomColumn/CustomColumn.tsx
@@ -1,22 +1,29 @@
 import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
+import { CustomButton } from '../../Atoms/Button';
+import { Etiqueta } from '../../Atoms/Label';
 
 
 interface CustomColumnProps {
   field: string;
   headerName: string;
   width?: number;
-  format: 'text' | 'number' | 'button';
+  format: 'text' | 'button' | 'toggle';
+  content:string;
+  variant: "text" | "outlined" | "contained";
+  color:"inherit" | "primary" | "secondary" | "success" | "error" | "info" | "warning";
 }
 
-const CustomColumn = ({ field, headerName, width = 150, format }: CustomColumnProps): GridColDef => ({
+const CustomColumn = ({ field, headerName, width = 150, format,  content, variant, color }: CustomColumnProps): GridColDef => ({
   field,
   headerName,
   width,
   renderCell: (params: GridRenderCellParams) => {
     if (format === 'button') {
-      return <CustomButton {...params}/>;
-    } else if (format == 'text') {
-      return <CustomInput {...params}/>;
+      return <CustomButton content={content} variant={variant} color={color}/>;
+    } else if (format === 'text') {
+      return <Etiqueta {...params}/>;
+    }else{
+      return <Etiqueta {...params}/>;
     }
   }
 });
