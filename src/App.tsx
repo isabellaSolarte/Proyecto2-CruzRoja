a71f744f1511aadb2baa06f6e92d11e3306aaa44
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2024-04-15 16:34:04 -0500

    Eliminar componente de búsqueda en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { Card, CardContent, CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './configs';
 import { Router } from './router';
-import Search from './components/Atoms/Search/Search';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router />
-      <Card sx={{ backgroundColor: '#f5f5f5' }}>
-        <CardContent>
-          <Search />
-        </CardContent>
-      </Card>
     </ThemeProvider>
   );
 };
