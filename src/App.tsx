fd0ecad9cd4af5732a2253ede2ba253ab16c94ed
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2024-04-15 16:20:19 -0500

    Search sin logica en handleSearch (+pruena en App)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Card, CardContent, CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './configs';
 import { Router } from './router';
 import Search from './components/Atoms/Search/Search';
@@ -8,7 +8,11 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router />
-      <Search />
+      <Card sx={{ backgroundColor: '#f5f5f5' }}>
+        <CardContent>
+          <Search />
+        </CardContent>
+      </Card>
     </ThemeProvider>
   );
 };
--- src/components/Atoms/Search/Search.tsx
+++ src/components/Atoms/Search/Search.tsx
@@ -2,22 +2,35 @@ import { IconButton, InputBase, Paper } from "@mui/material";
 import { useState } from "react";
 import ClearIcon from '@mui/icons-material/Clear';
 import SearchIcon from '@mui/icons-material/Search';
-import CustomButton from "../Button/Button";
+
 
 
 const Search: React.FC = () => {
   const [searchText, setSearchText] = useState('');
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(event.target.value);
+    //console.log('Guardando busqueda:', searchText);
   };
   const handleClearSearch = () => {
     setSearchText('');
+    //console.log('Limpiando búsqueda:', searchText);
   };
+  const handleSearch = () => {
+    // Lógica para realizar la búsqueda
+    console.log('Realizando búsqueda:', searchText);
+  };
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
+      handleSearch();
+    }
+  };
+
   return (
     <div>
       <Paper
         component="form"
-        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 'fullWidth' }}
       >
         {searchText && (
           <IconButton onClick={handleClearSearch}>
@@ -30,12 +43,12 @@ const Search: React.FC = () => {
           inputProps={{ 'aria-label': 'Buscar' }}
           value={searchText}
           onChange={handleSearchChange}
+          onKeyDown={handleKeyPress}
         />
-        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <IconButton type="button" sx={{ p: '10px' }} aria-label="search" onClick={handleSearch}>
           <SearchIcon />
         </IconButton>
       </Paper>
-      <CustomButton content="Search" variant="contained" color="primary" />
     </div>
   );
 };
