111292d25ea30acbf2956d6b45efecc80647a962
Daniela <driascos@unicauca.edu.co>
2024-04-15 18:25:38 -0500

    Clean App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './configs';
-import LabelGroup from './components/Molecules/CustomLabelGroup/CustomLabelGroup';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
+
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <LabelGroup texto1='Email' texto2='driacos@unicauca.edu.co' icon={<AccountBoxIcon />}/>
     </ThemeProvider>
   );
 };
