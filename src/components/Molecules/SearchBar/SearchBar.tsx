f2961b7e002c2f959ff1781a7889e99811e34836
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2024-04-15 19:30:05 -0500

    Agregar documentación al componente de barra de búsqueda

--- src/components/Molecules/SearchBar/SearchBar.tsx
+++ src/components/Molecules/SearchBar/SearchBar.tsx
@@ -8,6 +8,32 @@ interface SearchBarProps {
     additionalElements?: ReactNode;
 }
 
+/**
+ * Componente de barra de búsqueda.
+ * @component
+ * @example
+ * ```tsx
+ * <SearchBar
+ *  placeholder="Buscar..."
+ * onSearch={handleSearch}
+ * additionalElements={
+ *  <>
+ *   <IconButton onClick={handleAdd} aria-label="Agregar">
+ *    <AddCircleIcon />
+ *  </IconButton>
+ * <IconButton aria-label="Ajustes">
+ * <SettingsIcon />
+ * </IconButton>
+ * </>
+ * }
+ * />
+ * ```
+ * @param {string} placeholder - El texto de marcador de posición para la barra de búsqueda.
+ * @param {Function} onSearch - La función que se ejecuta cuando se realiza una búsqueda.
+ * @param {ReactNode} additionalElements - Elementos adicionales que se muestran junto a la barra de búsqueda a su derecha.
+ * 
+ * @returns {ReactElement} El componente de barra de búsqueda.
+ */
 const SearchBar: React.FC<SearchBarProps> = ({ placeholder, onSearch, additionalElements }) => {
 
     const handleSearch = (searchText: string) => {
