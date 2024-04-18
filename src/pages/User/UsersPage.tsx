b11b131e4752c77526b44d4f6e024ec35c30e7a3
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2024-04-18 12:47:34 -0500

    falta DataTable, se add textos a commons

--- src/pages/User/UsersPage.tsx
+++ src/pages/User/UsersPage.tsx
@@ -1,6 +1,8 @@
 import { Grid } from '@mui/material';
 import { CustomText, CustomButton, ManagmentLayout, SearchBar, DataTable, CustomColumn } from '../../components';
 import { useTranslation } from 'react-i18next';
+import EditIcon from '@mui/icons-material/Edit';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
 
 
@@ -8,41 +10,45 @@ import { useTranslation } from 'react-i18next';
 
 const UsersPage = () => {
   const { t } = useTranslation('commons');
-  const columns: GridColDef[] = [
-
+  const columns = [
+    CustomColumn({ field: 'names', headerName: t('usersPages.userTable.name'), width: 250, format: 'text', variante: 'texto', content: '', buttonDetails: []  }),
+    CustomColumn({ field: 'rol', headerName: t('usersPages.userTable.rol'), width: 250, format: 'text', variante: 'texto', content: '', buttonDetails: []  }),
+    CustomColumn({ field: 'actions', headerName: t('usersPages.userTable.actions'), width:250, format: 'button', variante: 'texto', content: '', buttonDetails: [
+      {
+        content: t('generalButtonText.edit'),
+        variant: 'contained',
+        color: 'primary',
+        icon: <EditIcon />
+      },
+      {
+        content: t('generalButtonText.view'),
+        variant: 'contained',
+        color: 'warning',
+        icon: <VisibilityIcon />
+      }
+    ] }),
+    CustomColumn({ field: 'state', headerName: t('generalButtonText.state'), format: 'switch', variante: 'texto', content: '', buttonDetails: [] })
   ];
   
   return (
     <ManagmentLayout
       title={<CustomText texto={t('pageTitles.users')} variante="titulo" />}
-      description={
-        <CustomText
-          texto={
-            'simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-          }
-          variante="texto"
-        />
-      }
-      inputBar={<SearchBar placeholder={t('rolesPages.roleForm.name')} />}
-      generalContents={
-        <Grid container spacing={2} direction={'column'} gap={5}>
-          <Grid item xs={12} sm={6}>
-            <CustomText texto={t('usersPages.userForm.name')} variante="texto" mandatory />
-          </Grid>
-
-          
-          <Grid item xs={12}>
-          <DataTable/>
-          </Grid>
-        </Grid>
-      }
-      button={
+      actionsContent={
         <CustomButton
-          content={t('generalButtonText.save')}
+          content={t('generalButtonText.create')}
           onClick={() => {}}
           variant="contained"
           color="success"
         />
+      }
+      inputBar={<SearchBar placeholder={t('generalButtonText.search')} />}
+      generalContents={
+        <Grid container spacing={2} direction={'column'} gap={5}>
+          <Grid item xs={12}>
+            {/* <DataTable columns={columns} rows={[]} /> */}
+            
+          </Grid>  
+        </Grid>
       }
     />
   );
