2499da5a2f13ccdc736b3e16d71291bef7241c61
Kevin Jimenez <kevin.stiven.jimenez@gmail.com>
2024-04-19 16:17:43 -0500

    Añadir navegación a las páginas de edición y visualización de usuarios en UsersPage.tsx

--- src/pages/User/UsersPage.tsx
+++ src/pages/User/UsersPage.tsx
@@ -1,8 +1,10 @@
-import { Grid } from '@mui/material';
 import { CustomText, CustomButton, ManagmentLayout, SearchBar, DataTable, CustomColumn } from '../../components';
 import { useTranslation } from 'react-i18next';
 import EditIcon from '@mui/icons-material/Edit';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import { PathNames } from '../../core/PathNames'
+import { useNavigate } from 'react-router-dom';
+
 
 
 
@@ -10,6 +12,18 @@ import VisibilityIcon from '@mui/icons-material/Visibility';
 
 const UsersPage = () => {
   const { t } = useTranslation('commons');
+  const navigate = useNavigate(); // Utilize the useNavigate hook
+  const handleEditButtonClick = (rowData: { documentNumber: number }) => {
+    console.log('¡Botón de editar clickeado! Implementa tu lógica de formulario aquí.');
+    const userId = rowData.documentNumber;
+    navigate(`${PathNames.EDIT_USER}/${String(userId)}`); // Convert userId to string
+  };
+
+  const handleViewButtonClick = (rowData: { documentNumber: number }) => {
+    console.log('¡Botón de ver clickeado! Implementa tu lógica de formulario aquí.');
+    const userId = rowData.documentNumber;
+    navigate(`${PathNames.VIEW_USER}/${String(userId)}`);
+  };
   const columns = [
     CustomColumn({ field: 'names', headerName: t('usersPages.userTable.name'), width: 250, format: 'text', variante: 'texto', content: '', buttonDetails: []  }),
     CustomColumn({ field: 'rol', headerName: t('usersPages.userTable.rol'), width: 150, format: 'text', variante: 'texto', content: '', buttonDetails: []  }),
@@ -18,25 +32,31 @@ const UsersPage = () => {
         content: t('generalButtonText.edit'),
         variant: 'contained',
         color: 'info',
-        icon: <EditIcon />
+        icon: <EditIcon />,
+        // onClick: handleEditButtonClick,
       },
       {
         content: t('generalButtonText.view'),
         variant: 'contained',
         color: 'warning',
-        icon: <VisibilityIcon />
+        icon: <VisibilityIcon />,
+        // onClick: handleViewButtonClick,
+
       }
     ] }),
     CustomColumn({ field: 'state', headerName: t('generalButtonText.state'), format: 'switch', variante: 'texto', content: '', buttonDetails: [] })
   ];
-  
+  const handleCreateButtonClick = () => {
+    navigate(PathNames.VIEW_USER);
+
+  };
   return (
     <ManagmentLayout
       title={<CustomText texto={t('pageTitles.users')} variante="titulo" />}
       actionsContent={
         <CustomButton
           content={t('generalButtonText.create')}
-          onClick={() => {}}
+          onClick={handleCreateButtonClick}
           variant="contained"
           color="success"
         />
