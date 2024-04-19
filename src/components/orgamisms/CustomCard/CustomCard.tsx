520d3bea93f1af60d6399bd4eca971d4ad8d050c
IsabellaSolarte <isabsolarte@unicauca.edu.co>
2024-04-19 00:57:44 -0500

    Ajustes a CustomCard, correciones en el commons con mayusculas y minúsculas

--- src/components/orgamisms/CustomCard/CustomCard.tsx
+++ src/components/orgamisms/CustomCard/CustomCard.tsx
@@ -1,17 +1,26 @@
 import React, { ReactElement } from "react";
 import LabelGroup from "../../Molecules/CustomLabelCard/CustomLabelCard";
-import CustomSwitch from "../../Switch/CustomSwitch";
+import { CustomSwitch } from "../../Atoms";
 import Card from '@mui/material/Card';
 import './CustomCardStyle.css';
 
-
+/*Ejemplo para usar
+    <CustomCard 
+      texto1="Crear empresa"
+      icon={<SearchIcon />}
+      sx={{ backgroundColor: theme.backgroundContentColors?.green}}
+      switchState={switchState}
+      handleSwitchState={handleSwitchState}
+    /> */
 interface CardCustomProps{
     texto1: string;
     texto2?: string;
     icon?: ReactElement;
     sx?: React.CSSProperties; 
+    switchState: boolean;
+    handleSwitchState: () => void;
 }
-const CustomCard = ({texto1,texto2,icon,sx}:CardCustomProps) =>{
+const CustomCard = ({texto1,texto2,icon,sx,switchState, handleSwitchState}:CardCustomProps) =>{
     return(
         <Card className = "card" 
             sx={{ maxWidth: 700, borderRadius: 5, ...sx }}>
@@ -23,7 +32,7 @@ const CustomCard = ({texto1,texto2,icon,sx}:CardCustomProps) =>{
                     texto2={texto2}
                 />
             </div>
-                <CustomSwitch />
+            <CustomSwitch switchState={switchState} handleSwitchState={handleSwitchState} />
         </Card>
             
     );
