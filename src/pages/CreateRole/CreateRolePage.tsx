a966523ce344c51161c8d754ff78319ca6675478
IsabellaSolarte <isabsolarte@unicauca.edu.co>
2024-04-19 13:51:41 -0500

    Se adelanta pagina de crear el Rol

--- src/pages/CreateRole/CreateRolePage.tsx
+++ src/pages/CreateRole/CreateRolePage.tsx
@@ -1,9 +1,10 @@
-// TODO: CREAR EL FORMULARIO CON VALIDACIONES
-
-import DataTable from "../../components/orgamisms/DataTable/DataTable";
-import { columns1 } from "../../configs/tablas/Columns";
 import { esES } from '@mui/material/locale';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useTranslation } from "react-i18next";
+import { ManagmentLayout, TabsAtomComponent,CustomButton,CustomText, CustomInput } from "../../components";
+import { Container,Box,useTheme } from '@mui/material';
+import { BorderTop } from '@mui/icons-material';
+import { Theme } from '@mui/material/styles';
 
 
 const theme = createTheme(
@@ -14,11 +15,36 @@ const theme = createTheme(
 );
 
 const CreateRolePage = () => {
-  // const { t } = useTranslation('commons');
-
-  return <ThemeProvider theme={theme}>
-    <DataTable enableCheckboxSelection={false} dataColumns={columns1} />
-  </ThemeProvider>;
+  const { t } = useTranslation('commons');
+  const theme = useTheme<Theme>();
+  return (
+    <ManagmentLayout
+      title={<CustomText texto={t('pageTitles.createRole')} variante="titulo" />}
+      actionsContent={
+        <CustomButton
+          content={t('generalButtonText.save')}
+          onClick={() => {}}
+          variant="contained"
+          color="success"
+        />
+      }
+      generalContents={
+        <Container>
+          <TabsAtomComponent tabContentItem={[t('rolesPages.tabs.visualizar'), t('rolesPages.tabs.permissions')]} />
+          <Box mt={5}>
+          <CustomText texto={t('rolesPages.roleForm.name')} variante="subtitulo" mandatory />
+          <CustomInput placeholder="Nombre rol" size="medium" />
+          </Box>
+          
+          <Box mt={8} sx={{borderTop: '1px solid #C8C8C8'}}> 
+            <CustomText texto={t('rolesPages.roleForm.color')} variante="subtitulo" mandatory />
+          </Box>
+        </Container>
+        
+      }
+      
+    />
+  );
 };
 
 export default CreateRolePage;
