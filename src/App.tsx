a0756c5546b336acd1e695eb4cf8068606bbed14
Daniela <driascos@unicauca.edu.co>
2024-04-22 19:54:52 -0500

    Change app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './configs';
 import { Router } from './router';
-import AccordionComponent from './components/Molecules/CustomAccordion/CustomAccordion';
+
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AccordionComponent accordionSummary={'sisas'} contentAccordion={<div>hola</div>} />
+      <Router />
     </ThemeProvider>
   );
 };
