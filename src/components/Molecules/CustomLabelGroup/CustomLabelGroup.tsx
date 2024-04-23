fb0e7a804d4e624249f8c784c0efb6c974c7991e
Daniela <driascos@unicauca.edu.co>
2024-04-22 19:44:46 -0500

    Check accordion, to-do styles

--- src/components/Molecules/CustomLabelGroup/CustomLabelGroup.tsx
+++ src/components/Molecules/CustomLabelGroup/CustomLabelGroup.tsx
@@ -1,17 +1,16 @@
 import { ReactElement } from "react";
-import Etiqueta from "../../Atoms/Label/Typography";
-
+import CustomText from "../../Atoms/Label/Typography";
 interface labelGroupProps{
     texto1: string;
     texto2: string;
     icon?:ReactElement;
 }
+
 const LabelGroup = ({texto1,texto2,icon}:labelGroupProps) => {
-  console.log(texto2)
     return (
         <>
-        <Etiqueta texto={texto1} icon={icon} variante="texto" color="gris" />
-        <Etiqueta texto={texto2} variante="texto"  />
+        <CustomText texto={texto1} icon={icon} variante="texto"   />
+        <CustomText texto={texto2} variante="texto"  />
         </> 
       
     );
