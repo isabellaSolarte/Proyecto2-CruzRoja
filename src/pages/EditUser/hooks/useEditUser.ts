ded8d19ec635a7faa7dbca1a539799101f9ffa2e
dmesa <dm31284@gmail.com>
2024-04-28 18:08:57 -0500

    ajuste peticiónes get voluntarios y compañias

--- src/pages/EditUser/hooks/useEditUser.ts
+++ src/pages/EditUser/hooks/useEditUser.ts
@@ -4,54 +4,48 @@ import {
   UserModel,
   VolunterUserModel,
 } from '../../../models';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { CompanyUserEditType, VolunteerEditType } from '../types';
-import { getCompayUserById, getVolunteerById, putUserCompany, putVolunteer } from '../../../services';
+import {
+  getCompayUserById,
+  getVolunteerById,
+  putUserCompany,
+  putVolunteer,
+} from '../../../services';
 import { PathNames } from '../../../core';
 import { useNavigate, useParams } from 'react-router-dom';
 export const useEditUser = () => {
   const { type, id } = useParams();
   const [userData, setuserData] = useState<
-  CompanyUserModel | VolunterUserModel | undefined
+    CompanyUserModel | VolunterUserModel | undefined
   >(undefined);
-  const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState(false); // Estado para indicar si los datos se están cargando
   const navigate = useNavigate();
   const [roles, setRoles] = useState<RoleModel[]>([]);
-  const { 
-    handleSubmit,
-    register,
-    getValues,
-    setValue
-   } = useForm<
-      UserModel | VolunteerEditType | CompanyUserEditType
-    >({
-      defaultValues: useMemo(() => {
-        return userData;
-      }, [userData]),
-      resolver: undefined,
-    });
-    
-  const onSubmit = async (data :any) => {
-    const updateUser = {
-      ...getValues(),
-    }
-    try{
-      if(type=='volunteer'){
-         await putVolunteer(data)
-      }else{
-        await putUserCompany(data)
+  const { handleSubmit, register } = useForm<
+    UserModel | VolunteerEditType | CompanyUserEditType
+  >({
+    defaultValues: useMemo(() => {
+      return userData;
+    }, [userData]),
+    resolver: undefined,
+  });
+
+  const onSubmit = async (data: any) => {
+    try {
+      if (type == 'volunteer') {
+        await putVolunteer(data);
+      } else {
+        await putUserCompany(data);
       }
+    } catch (error) {
+      console.log(error);
     }
-    catch(error: any){
-
-    }
-  }
-  const loadVolunteerUserDataToEdit = async (userId:number) => {
+  };
+  const loadVolunteerUserDataToEdit = async (userId: number) => {
     try {
-      let response;
-      response = await getVolunteerById(userId);
+      const response = await getVolunteerById(userId);
       setuserData(response);
       setLoading(false);
     } catch (error) {
@@ -59,10 +53,9 @@ export const useEditUser = () => {
     }
   };
 
-  const loadCompanyUserDataToEdit = async (userId:number) => {
+  const loadCompanyUserDataToEdit = async (userId: number) => {
     try {
-      let response;
-      response = await getCompayUserById(userId);
+      const response = await getCompayUserById(userId);
       setuserData(response);
       setLoading(false);
     } catch (error) {
@@ -70,44 +63,48 @@ export const useEditUser = () => {
     }
   };
 
- 
-  const handleCancel = ( ) =>
-    {
-      const userIdString = userData?.id ? userData.id.toString() : '';
+  const loadUserData = async (userId: number) => {
+    if (type === 'volunteer') {
+      await loadVolunteerUserDataToEdit(userId);
+    } else {
+      await loadCompanyUserDataToEdit(userId);
+    }
+  };
 
-      navigate(
-        PathNames.VIEW_USER.replace(':id', userIdString).replace(
-          ':type',
-          type ? type : 'volunteer',
-        ),
-        {
-          replace: true,
-        },
-      );
-    };
-    const handleSave = ( ) =>
+  const handleCancel = () => {
+    const userIdString = userData?.id ? userData.id.toString() : '';
+
+    navigate(
+      PathNames.VIEW_USER.replace(':id', userIdString).replace(
+        ':type',
+        type ? type : 'volunteer',
+      ),
       {
-        const userIdString = userData?.id ? userData.id.toString() : '';
-        navigate(
-          PathNames.VIEW_USER.replace(':id', userIdString).replace(
-            ':type',
-            type ? type : 'volunteer',
-          ),
-          {
-            replace: true,
-          },
-        );
-      };
-      
+        replace: true,
+      },
+    );
+  };
+  const handleSave = () => {
+    const userIdString = userData?.id ? userData.id.toString() : '';
+    navigate(
+      PathNames.VIEW_USER.replace(':id', userIdString).replace(
+        ':type',
+        type ? type : 'volunteer',
+      ),
+      {
+        replace: true,
+      },
+    );
+  };
+
   return {
     userData,
     roles,
-    loadCompanyUserDataToEdit,
-    loadVolunteerUserDataToEdit,
+    loadUserData,
     handleSave,
     handleCancel,
     onSubmit,
     handleSubmit,
-    register
+    register,
   };
 };
