723fe55e406cd2068f97011e13b3d96e016e9ab4
IsabellaSolarte <isabsolarte@unicauca.edu.co>
2024-05-03 02:13:07 -0500

    Ver error del mení

--- src/components/Molecules/DrawerMenu/DrawerMenu.tsx
+++ src/components/Molecules/DrawerMenu/DrawerMenu.tsx
@@ -69,7 +69,6 @@ const DrawerMenu = ({ options }: CollapseMenuProps) => {
     PathNames.PERMISSIONS,
     PathNames.SETTINGS,
     PathNames.CLOSE_SESSION,
-    PathNames.COMPONETS,
     PathNames.CATEGORIES,
   ];
   const drawerOptions = options.filter(option => availableDrawerOptions.includes(option.path));
