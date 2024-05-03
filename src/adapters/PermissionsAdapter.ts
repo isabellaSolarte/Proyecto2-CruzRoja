6be5eafdc3e7946dfb63312647ea30e89ad61d7a
dmesa <dm31284@gmail.com>
2024-05-02 22:45:19 -0500

    ajuste

--- src/adapters/PermissionsAdapter.ts
+++ src/adapters/PermissionsAdapter.ts
@@ -3,9 +3,10 @@ import { PermissionModel } from '../models';
 export const PermissionAdapter = (externPermission: any): PermissionModel => {
   return {
     id: externPermission.idPermission,
-    name: PermissionNames[
-      externPermission.name as keyof typeof PermissionNames
-    ],
+    name: externPermission.name,
+    // name: PermissionNames[
+    //   externPermission.name as keyof typeof PermissionNames
+    // ],
     description: externPermission.description,
   };
 };
