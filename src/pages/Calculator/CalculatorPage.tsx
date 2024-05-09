5f83b05a62512e84a11bc26d86285f6869f15964
dmesa <dm31284@gmail.com>
2024-05-09 15:08:58 -0500

    ajuste para desarrollo

--- src/pages/Calculator/CalculatorPage.tsx
+++ src/pages/Calculator/CalculatorPage.tsx
@@ -3,7 +3,7 @@ import { CustomStepper } from '../../components';
 const CalculatorPage = () => {
   return (
     <CustomStepper stepsData={[]} activeStep={0}>
-      <div></div>
+      <div>{/* AQUI PONER SUS FORMULARIOS POR AHORA PARA QUE PRUEBEN */}</div>
     </CustomStepper>
   );
 };
