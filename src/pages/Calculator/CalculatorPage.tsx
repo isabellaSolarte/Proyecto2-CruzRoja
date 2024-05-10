cf6f145f138c403bcc57cab3513c0072938979de
anfherrera <anfherrera@unicauca.edu.co>
2024-05-10 17:07:06 -0500

    Vista calculadora

--- src/pages/Calculator/CalculatorPage.tsx
+++ src/pages/Calculator/CalculatorPage.tsx
@@ -1,9 +1,28 @@
-import { CustomStepper } from '../../components';
+import { Box, Typography } from '@mui/material';
+import { CustomButton, CustomStepper } from '../../components';
 
 const CalculatorPage = () => {
   return (
     <CustomStepper stepsData={[]} activeStep={0}>
-      <div>{/* AQUI PONER SUS FORMULARIOS POR AHORA PARA QUE PRUEBEN */}</div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Box sx={{ width: '50%', textAlign: 'center' }}>
+              <img
+                src="/icono-calculadora.png"
+                alt="Calculadora"
+                style={{ maxWidth: '100%', height: 'auto' }}
+              />
+            </Box>
+            <Typography variant="h2" sx={{ mt: 4, mb: 2 }}>
+              Calculadora de huella de carbono
+            </Typography>
+            <CustomButton
+              variant="contained"
+              color="success"
+              sx={{ mt: 3, mb: 2 }}
+              content={'Iniciar'}
+              type="submit"
+            />
+          </div>
     </CustomStepper>
   );
 };
