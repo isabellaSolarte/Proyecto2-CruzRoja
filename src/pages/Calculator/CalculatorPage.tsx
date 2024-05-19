5439cb284cfe1300c04bb49558ccdbd72fd9e284
anfherrera <anfherrera@unicauca.edu.co>
2024-05-19 11:55:09 -0500

    se agrego mangament layout en calculatorPage

--- src/pages/Calculator/CalculatorPage.tsx
+++ src/pages/Calculator/CalculatorPage.tsx
@@ -1,5 +1,5 @@
 import { Box, Grid } from '@mui/material';
-import { CustomButton, CustomStepper, CustomText } from '../../components';
+import { CustomButton, CustomStepper, CustomText, ManagmentLayout } from '../../components';
 import { CoverageForm, useCoverageForm } from './CoverageForm';
 import { useCalculatorHook, useStepper } from './hooks';
 import { useEffect } from 'react';
@@ -21,37 +21,43 @@ const CalculatorPage = () => {
   }, [calculator.formHasErrors]);
 
   const handleClick = () => {
+    console.log('Current step:', currentStep);
+    console.log('Form has errors:', calculator.formHasErrors);
+  
     const stepsFunctions: { [key: number]: () => void } = {
       1: () => {
-        // funciónParaPaso1();
-        handleNextStep();
+        if (!calculator.formHasErrors) {
+          handleNextStep();
+        }
       },
       2: () => {
-        // funciónParaPaso2();
-        handleNextStep();
+        if (!calculator.formHasErrors) {
+          handleNextStep();
+        }
       },
       3: () => {
-        // funciónParaPaso3();
-        handleNextStep();
+        handleCoverageFormData();
+        if (!calculator.formHasErrors) {
+          handleNextStep();
+        }
       },
       4: () => {
-        handleCoverageFormData();
-        handleNextStep();
+        if (!calculator.formHasErrors) {
+          handleNextStep();
+        }
       },
       5: () => {
-        // funciónParaPaso5();
-        handleNextStep();
+        if (!calculator.formHasErrors) {
+          handleNextStep();
+        }
       },
       6: () => {
-        // funciónParaPaso6();
-        handleNextStep();
-      },
-      7: () => {
-        // funciónParaPaso7();
-        handleNextStep();
+        if (!calculator.formHasErrors) {
+          handleNextStep();
+        }
       },
     };
-
+  
     if (stepsFunctions[currentStep]) {
       stepsFunctions[currentStep]();
     } else {
@@ -60,67 +66,77 @@ const CalculatorPage = () => {
   };
 
   return (
-    <CustomStepper stepsData={stepList} activeStep={currentStep}>
-      {currentStep === 0 && (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <Box sx={{ width: '20%', textAlign: 'center' }}>
-            <img
-              src="/icono-calculadora.png"
-              alt="Calculadora"
-              style={{ maxWidth: '100%', height: 'auto' }}
-            />
-          </Box>
-          <CustomText texto={t('pageTitles.calculator')} variante={'titulo'} />
-          <CustomButton
-            variant="contained"
-            color="success"
-            sx={{ mt: 3, mb: 2 }}
-            content={t('generalButtonText.start')}
-            onClick={handleNextStep}
-          />
-        </div>
-      )}
-      {currentStep > 0 && (
-        <div
-          style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}
-        >
-          <Grid sx={{ width: '100%', textAlign: 'center' }}>
-            <div>
-              {currentStep === 1 && <div> </div>}
-              {currentStep === 2 && <div> </div>}
-              {currentStep === 3 && <SourcesDataForm sources={sourcesDictionaryPrueba2} />}
-              {currentStep === 4 && <CoverageForm />}
-              {currentStep === 5 && <CostsAndUsageForm />}
-              {currentStep === 6 && <div> </div>}
-            
-            </div>
-          </Grid>
-          <Box
-            sx={{
-              width: '100%',
-              display: 'flex',
-              justifyContent: 'space-between',
-              padding: '0 20%',
-            }}
-          >
-            <CustomButton
-              variant="contained"
-              color="primary"
-              content={t('generalButtonText.back')}
-              onClick={handleStepBack}
-            />
-            {currentStep < stepList.length - 1 && (
+    <ManagmentLayout
+      title={<CustomText texto={t('pageTitles.calculator')} variante="titulo" />}
+      generalContents={
+        <CustomStepper stepsData={stepList} activeStep={currentStep}>
+          {currentStep === 0 && (
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', width: '100%' }}>
+              <Box sx={{ width: { xs: '50%', md: '20%' }, textAlign: 'center' }}>
+                <img
+                  src="/icono-calculadora.png"
+                  alt="Calculadora"
+                  style={{ maxWidth: '100%', height: 'auto' }}
+                />
+              </Box>
+              <CustomText texto={t('pageTitles.calculator')} variante={'titulo'} />
               <CustomButton
                 variant="contained"
-                color="info"
-                content={t('components.stepper.next')}
-                onClick={handleClick}
+                color="success"
+                sx={{ mt: 3, mb: 2 }}
+                content={t('generalButtonText.start')}
+                onClick={handleNextStep}
               />
-            )}
-          </Box>
-        </div>
-      )}
-    </CustomStepper>
+            </Box>
+          )}
+          {currentStep > 0 && (
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                width: '100%',
+                padding: { xs: '0 10%', md: '0 20%' },
+              }}
+            >
+              <Grid container spacing={2} sx={{ width: '100%', textAlign: 'center' }}>
+                <Grid item xs={12}>
+                  {currentStep === 1 && <div> </div>}
+                  {currentStep === 2 && <div> </div>}
+                  {currentStep === 3 && <SourcesDataForm sources={sourcesDictionaryPrueba2} />}
+                  {currentStep === 4 && <CoverageForm />}
+                  {currentStep === 5 && <CostsAndUsageForm />}
+                  {currentStep === 6 && <div> </div>}
+                </Grid>
+              </Grid>
+              <Box
+                sx={{
+                  width: '100%',
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  mt: 3,
+                }}
+              >
+                <CustomButton
+                  variant="contained"
+                  color="primary"
+                  content={t('generalButtonText.back')}
+                  onClick={handleStepBack}
+                />
+                {currentStep < stepList.length - 1 && (
+                  <CustomButton
+                    variant="contained"
+                    color="info"
+                    content={t('components.stepper.next')}
+                    onClick={handleClick}
+                  />
+                )}
+              </Box>
+            </Box>
+          )}
+        </CustomStepper>
+      }
+    />
   );
 };
 
