7a14cc5283971c49422386061e6b630e2beb2d4b
dmesa <dm31284@gmail.com>
2024-05-20 17:47:50 -0500

    ajuste

--- src/pages/Calculator/ValidateDataForm/ValidateForm.tsx
+++ src/pages/Calculator/ValidateDataForm/ValidateForm.tsx
@@ -1,58 +1,72 @@
 import { CustomButton, CustomText, ManagmentLayout } from '../../../components';
 import { Fragment } from 'react/jsx-runtime';
-import { Box, Grid } from '@mui/material';
+import { Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import useValidateForm from './hooks/useValidateForm';
 import FieldTable from './models/fieldTable';
-const ValidateForm = () =>
-{
-    const {  adaptedSources,handleSubmit,onSubmit} = useValidateForm();
 
-    const { t } = useTranslation('commons');
+interface ValidateFormProps {
+  nextStep: () => void;
+  stepBack: () => void;
+}
 
-    return(
-        <ManagmentLayout
-            title={<CustomText texto={t('calculator.stepper.validation')} variante={'titulo'} />}
-            
-            generalContents={
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    {adaptedSources.map((source , index) => (
-                        <Fragment key={index}>
-                         <CustomText texto={source.categoryName} variante={'titulo'} />
-                         <br />
-                         {source.pollutants.map((pollutant,index) =>(
-                            <div key={index}>
-                                <CustomText texto={pollutant.pollutantName} variante={'subtitulo'} />
-                                <br />
-                                {pollutant.sources.map((src,index) => (
-                                    <div key={index}>
-                                        <FieldTable labelInput1={src.name} labelInput2={`${src.coverage.informedSources}/${src.coverage.totalSources} und`} labelInput3={`${src.facturation.month}`} labelInput4={`Costo $ ${src.facturation.cost} COP`} />
-                                    </div>
-                                ))
+const ValidateForm = ({ stepBack }: ValidateFormProps) => {
+  const { adaptedSources, handleSubmit, onSubmit } = useValidateForm();
 
-                                }
-                            </div>
-                         ))}
-                       </Fragment>
-                    ))}
-                    <Box
-                        sx={{
-                            width: '100%',
-                            display: 'flex',
-                            justifyContent: 'flex-end', 
-                            padding: '0 20%', 
-                            marginTop: '7rem',
-                        }}
-                    >
-                        
-                    <Box sx={{ position: 'relative', top: '4rem' }}>
-                        <CustomButton content="Guardar" color="success" type="submit" />
-                    </Box>
-                    </Box>  
-                    
-                </form>
-            }
-        />
-    );
+  const { t } = useTranslation('commons');
+
+  return (
+    <ManagmentLayout
+      title={<CustomText texto={t('calculator.stepper.validation')} variante={'titulo'} />}
+      generalContents={
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {adaptedSources.map((source, index) => (
+            <Fragment key={index}>
+              <CustomText texto={source.categoryName} variante={'titulo'} />
+              <br />
+              {source.pollutants.map((pollutant, index) => (
+                <div key={index}>
+                  <CustomText texto={pollutant.pollutantName} variante={'subtitulo'} />
+                  <br />
+                  {pollutant.sources.map((src, index) => (
+                    <div key={index}>
+                      <FieldTable
+                        labelInput1={src.name}
+                        labelInput2={`${src.coverage.informedSources}/${src.coverage.totalSources} und`}
+                        labelInput3={src.facturation.month}
+                        labelInput4={`Costo $ ${src.facturation.cost} COP`}
+                      />
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </Fragment>
+          ))}
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'space-between',
+              mt: 3,
+            }}
+          >
+            <CustomButton
+              variant="contained"
+              color="primary"
+              content={t('generalButtonText.back')}
+              onClick={stepBack}
+            />
+
+            <CustomButton
+              variant="contained"
+              color="info"
+              content={t('components.stepper.next')}
+              type="submit"
+            />
+          </Box>
+        </form>
+      }
+    />
+  );
 };
 export default ValidateForm;
