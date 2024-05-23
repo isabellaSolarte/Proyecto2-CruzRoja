028823dd4dcbff995d6355e7e798882910ae5270
dmesa <dm31284@gmail.com>
2024-05-22 20:49:46 -0500

    ajuste commons

--- src/pages/Calculator/PollutionTypeForm/PollutionTypeForm.tsx
+++ src/pages/Calculator/PollutionTypeForm/PollutionTypeForm.tsx
@@ -1,10 +1,15 @@
-import { useEffect } from 'react';
-import {CustomText, ErrorText, ManagmentLayout, CustomAccordion, CustomIconDetails, CustomButton} from '../../../components';
-import { Grid,Box } from '@mui/material';
-import PollutionTypeCard from './components/PollutionTypeCard'; 
+import {
+  CustomText,
+  ErrorText,
+  ManagmentLayout,
+  CustomAccordion,
+  CustomIconDetails,
+  CustomButton,
+} from '../../../components';
+import { Grid, Box } from '@mui/material';
+import PollutionTypeCard from './components/PollutionTypeCard';
 import usePollutionTypeForm from './hooks/usePollutionTypeForm';
 import HelpIcon from '@mui/icons-material/Help';
-
 
 interface PollutionTypeFormProps {
   nextStep: () => void;
@@ -12,101 +17,87 @@ interface PollutionTypeFormProps {
 }
 
 const PollutionTypeForm = ({ nextStep, stepBack }: PollutionTypeFormProps) => {
-    
-    const { 
-      t,
-      adaptedPollutionTypes,
-      register,
-      updatePollutionType,
-      errors,
-      handleSubmit, 
-      onSubmit,
-      reset,
-    } = usePollutionTypeForm(nextStep)
-
-    useEffect(() => {
-    }, [errors]);
+  const {
+    t,
+    adaptedPollutionTypes,
+    register,
+    updatePollutionType,
+    errors,
+    handleSubmit,
+    onSubmit,
+  } = usePollutionTypeForm(nextStep);
 
   return (
-    <ManagmentLayout 
-    title={
-      <div style={{ display: 'flex', alignItems: 'center'}}>
-        <CustomText texto={t('calculator.pollutionTypeForm.title')} variante={'titulo'} />
+    <ManagmentLayout
+      title={<CustomText texto={t('calculator.pollutionTypeForm.title')} variante={'titulo'} />}
+      actionsContent={
         <CustomIconDetails
-          description={t('calculator.pollutionTypeForm.example')}
+          description={t('calculator.pollutionTypeForm.helpTypes')}
           icon={<HelpIcon color={'disabled'} />}
         />
-      </div>
       }
-    description={
-      <CustomText texto={t('calculator.pollutionTypeForm.description')} variante={'texto'} />
-    }
-    
-    generalContents={
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Box mt={5} sx={{ borderTop: '1px solid #C8C8C8'}}>
-        {adaptedPollutionTypes.map(categoria =>
-          <CustomAccordion
-          key={categoria.id}
-          accordionSummary={categoria.name}
-          contentAccordion={
-            <>
-              {
-                categoria.pollutans.map(pollutionType => (
-                  <PollutionTypeCard
-                    key={pollutionType.id}
-                    pollutionType={pollutionType}
-                    positiveAction={()=>{updatePollutionType(pollutionType.name, true)}}
-                    negativeAction={()=>{updatePollutionType(pollutionType.name, false)}}
-                    props={register('pollutionType')}
-                  />
-                ))
-              }
-            </>
-          }
-          />
-            
-          
-        )}
-
-          {!!errors.pollutionType && (
-            <Grid item xs={12}>
-              <ErrorText
-                error={errors.pollutionType.message}
-                formErrorKey={'calculator'}
+      description={
+        <CustomText texto={t('calculator.pollutionTypeForm.description')} variante={'texto'} />
+      }
+      generalContents={
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Box mt={5} sx={{ borderTop: '1px solid #C8C8C8' }}>
+            {adaptedPollutionTypes.map(categoria => (
+              <CustomAccordion
+                key={categoria.id}
+                accordionSummary={categoria.name}
+                contentAccordion={
+                  <>
+                    {categoria.pollutans.map(pollutionType => (
+                      <PollutionTypeCard
+                        key={pollutionType.id}
+                        pollutionType={pollutionType}
+                        positiveAction={() => {
+                          updatePollutionType(pollutionType.name, true);
+                        }}
+                        negativeAction={() => {
+                          updatePollutionType(pollutionType.name, false);
+                        }}
+                        props={register('pollutionType')}
+                      />
+                    ))}
+                  </>
+                }
               />
-            </Grid>
-          )}
-        </Box>
-        <Box
-          sx={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'space-between',
-            mt: 3,
-          }}
-        >
-          <CustomButton
-            variant="contained"
-            color="primary"
-            content={t('generalButtonText.back')}
-            onClick={stepBack}
-          />
+            ))}
 
-          <CustomButton
-            variant="contained"
-            color="info"
-            content={t('components.stepper.next')}
-            type="submit"
-          />
-        </Box>
-      </form>
-    }
-    >
-    </ManagmentLayout>
-      
+            {!!errors.pollutionType && (
+              <Grid item xs={12}>
+                <ErrorText error={errors.pollutionType.message} formErrorKey={'calculator'} />
+              </Grid>
+            )}
+          </Box>
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'space-between',
+              mt: 3,
+            }}
+          >
+            <CustomButton
+              variant="contained"
+              color="primary"
+              content={t('generalButtonText.back')}
+              onClick={stepBack}
+            />
+
+            <CustomButton
+              variant="contained"
+              color="info"
+              content={t('components.stepper.next')}
+              type="submit"
+            />
+          </Box>
+        </form>
+      }
+    ></ManagmentLayout>
   );
-  };
-  
+};
+
 export default PollutionTypeForm;
-  
