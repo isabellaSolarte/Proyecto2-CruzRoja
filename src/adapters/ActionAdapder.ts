8d2df68d7c9814f2e00f52368e5f32fccf800541
dmesa <dm31284@gmail.com>
2024-06-08 17:29:17 -0500

    listar planes

--- src/adapters/ActionAdapder.ts
+++ src/adapters/ActionAdapder.ts
@@ -1,6 +1,7 @@
 import { ActionsModel } from '../models/Actions';
 
 export const ActionAdapter = (externalAction: any): ActionsModel => {
+  console.log('externalAction', externalAction);
   return {
     id: externalAction.actionId,
     name: externalAction.actionName,
