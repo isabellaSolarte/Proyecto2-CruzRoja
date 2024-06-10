0b8873442ab3361a4a59bad91a068731c84d9731
Stiven1707 <kevin.stiven.jimenez@gmail.com>
2024-06-10 14:55:17 -0500

    chore: Update MainContent component styles and text content

--- src/pages/LandingPage/components/MainContent.tsx
+++ src/pages/LandingPage/components/MainContent.tsx
@@ -50,16 +50,19 @@ const MainContent: React.FC = () => {
 
   return (
     <BackgroundContainer>
-      <ContentContainer maxWidth="lg">
-      <Box sx={{ p: 2, marginLeft: { xs: 0, md: '25vw' } }}>
+      <ContentContainer maxWidth="md">
+      <Box sx={{ p: 2, marginLeft: { xs: 0, md: '30vw', lg: '20vw' } }}>
             <CustomText texto={t('CRUZ ROJA FOOTPRINT')} variante="titulo" styles={{textAlign: 'center',}}/>
             <Typography variant="body1" paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
+            La Herramienta de Gestión de la Información y Cálculo del Inventario de Gases de Efecto Invernadero (GEI) busca proporcionar a las empresas una solución integral para la gestión y reporte de sus emisiones de carbono. Cumple con los estándares de la NTC 14064-1 y el Protocolo GHG, facilitando la identificación, cuantificación y reporte de fuentes de emisión de GEI.
             </Typography>
             <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2, mb: 4 }}>
-            <Button variant="contained" color="info" sx={{ borderRadius: '50px' }}>
-                USAR CALCULADORA
-            </Button>
+            <Button 
+                variant="contained" 
+                color="info" 
+                sx={{ borderRadius: '50px' }}
+                onClick={() => {navigate(PathNames.LOGIN)}}
+                >USAR CALCULADORA</Button>
             <Button 
                 color="inherit" 
                 sx={{ borderRadius: '50px', border: `2px solid ${theme.palette.success.main}` }}
@@ -83,7 +86,7 @@ const MainContent: React.FC = () => {
           <BarChartIcon color="success" fontSize="large" />
           <CustomText texto={t('Estadísticas')} variante="subtitulo" styles={{textAlign: 'center',}}/>
             <Typography variant="body2">
-              Our frontend developers understand the delicate balance between aesthetics and functionality.
+            La herramienta ofrece estadísticas detalladas sobre las emisiones de GEI, facilitando la toma de decisiones informadas y el seguimiento de objetivos ambientales.
             </Typography>
           </InfoContainer>
         </Grid>
@@ -92,7 +95,7 @@ const MainContent: React.FC = () => {
           <BusinessIcon color='success' fontSize="large" />
           <CustomText texto={t('Empresas')} variante="subtitulo" styles={{textAlign: 'center',}}/>
             <Typography variant="body2">
-              Our backend developers are the architects of efficiency and security. They design and build the databases and APIs.
+            Diseñada para empresas de todos los tamaños, esta herramienta ayuda a gestionar y reducir la huella de carbono, mejorando la sostenibilidad y cumpliendo con las normativas ambientales.
             </Typography>
           </InfoContainer>
         </Grid>
@@ -101,7 +104,7 @@ const MainContent: React.FC = () => {
           <ThumbUpIcon color="success" fontSize="large" />
           <CustomText texto={t('Beneficios')} variante="subtitulo" styles={{textAlign: 'center',}}/>
             <Typography variant="body2">
-              Our data analytics team is a blend of mathematicians, statisticians, and technology enthusiasts.
+            Además de cumplir con las regulaciones, las empresas pueden mejorar su imagen corporativa, optimizar costos y contribuir significativamente a la lucha contra el cambio climático.
             </Typography>
           </InfoContainer>
         </Grid>
